ffa2d731ca2ce8e61922e3a050a0fe2237fe29af
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-09-29 10:45:11 +0200

    v1.0

--- view/login.php
+++ view/login.php
@@ -4,13 +4,13 @@
                 <h3 class="font-montserrat cover-heading mb20 mt20">Inicio sesión</h3>
                 <form class="clearfix mb35">
                     <div class="col-sm-8 col-sm-offset-2">
-                        <input type="email" name="email" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Email">
+                        <input id="login-email" type="email" name="email" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Email">
                     </div>
                     <div class="col-sm-8 col-sm-offset-2 mt10">
-                        <input type="password" name="password" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Password">
+                        <input id="login-password" type="password" name="password" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Password">
                     </div>
                     <div class="col-sm-8 col-sm-offset-2 mt5">
-                        <button class="button button-lg button-circle button-block button-pasific hover-ripple-out">Iniciar</button><br><br>
+                        <button id="btn-login" class="button button-lg button-circle button-block button-pasific hover-ripple-out">Iniciar</button><br><br>
                         <a href="#" class="color-dark mt20" id="showFormRegister">¿No tienes cuenta? Regístrate</a>
                     </div>
                 </form>
@@ -22,13 +22,13 @@
                 <h3 class="font-montserrat cover-heading mb20 mt20">Crear cuenta</h3>
                 <form class="clearfix mb35">
                     <div class="col-sm-8 col-sm-offset-2">
-                        <input type="email" name="email" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Email">
+                        <input id="register-email" type="email" name="email" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Email">
                     </div>
                     <div class="col-sm-8 col-sm-offset-2 mt10">
-                        <input type="password" name="password" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Password">
+                        <input id="register-password" type="password" name="password" class="form-control text-center no-border input-lg input-circle bg-gray2" placeholder="Password">
                     </div>
                     <div class="col-sm-8 col-sm-offset-2 mt5">
-                        <button class="button button-lg button-circle button-block button-pasific hover-ripple-out">Regístrate</button><br><br>
+                        <button id="btn-register" class="button button-lg button-circle button-block button-pasific hover-ripple-out">Regístrate</button><br><br>
                         <a href="#" class="color-dark mt20" id="showFormLogin">¿Tienes una cuenta? Inicia sesión</a><br>
                     </div>
                 </form>
