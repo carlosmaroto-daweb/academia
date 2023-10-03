1158ea36d155323114912da9e4f614140d2d989a
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-03 14:51:57 +0200

    v1.0

--- config/config.php
+++ config/config.php
@@ -1,12 +1,15 @@
 <?php
+    // Constantes de la base de datos
     define("DB_HOST", "localhost");
     define("DB", "academia");
     define("DB_USER", "u-Area14");
     define("DB_PASS", "u-Area14");
 
+    // Constantes de la ruta por defecto
     define("DEFAULT_CONTROLLER", "userController");
     define("DEFAULT_ACTION", "index");
 
+    // Constantes del encriptamiento
     define("PASSWORD_HASH", PASSWORD_DEFAULT);
     define("PASSWORD_COST", 14);
 ?>
