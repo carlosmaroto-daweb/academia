582897ec04b808675b2d031290a712900f1e2b21
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-08 11:18:54 +0200

    v1.0

--- model/course.php
+++ model/course.php
@@ -1,16 +1,17 @@
 <?php
     /* 
-    * Esta clase forma la estructura de datos que puede tener un módulo,
-    * es utilizada para instanciar módulos y poder manejar sus datos.
+    * Esta clase forma la estructura de datos que puede tener un curso,
+    * es utilizada para instanciar cursos y poder manejar sus datos.
     * 
     * @author: Carlos Maroto
     * @version: v1.0.0 Carlos Maroto
     */
-    class Module {
+    class Course {
 
         // Atributos
         private $id;
         private $name;
+        private $studies;
         private $header_image;
         private $preview;
         private $content;
@@ -18,15 +19,17 @@
         /*
          * Constructor que inicializa todos los atributos a los pasados como argumento.
          * 
-         * @param id             Int id del módulo
-         * @param name           String nombre del módulo
-         * @param header_image   String imagen de cabecera del módulo
-         * @param preview        String contenido de previsualización del módulo
-         * @param content        String contenido del módulo
+         * @param id             Int id del curso
+         * @param name           String nombre del curso
+         * @param studies        JSON estudios del curso
+         * @param header_image   String imagen de cabecera del curso
+         * @param preview        String contenido de previsualización del curso
+         * @param content        String contenido del curso
         */
-        function __construct($id, $name, $header_image, $preview, $content) {
+        function __construct($id, $name, $studies, $header_image, $preview, $content) {
             $this->id           = $id;
             $this->name         = $name;
+            $this->studies      = $studies;
             $this->header_image = $header_image;
             $this->preview      = $preview;
             $this->content      = $content;
@@ -35,7 +38,7 @@
         /*
         * Método de consulta del atributo id.
         * 
-        * @return Devuelve el id del módulo
+        * @return Devuelve el id del curso
         */
         function getId() {
             return $this->id;
@@ -44,16 +47,25 @@
         /*
         * Método de consulta del atributo name.
         * 
-        * @return Devuelve el nombre del módulo
+        * @return Devuelve el nombre del curso
         */
         function getName() {
             return $this->name;
         }
 
         /*
+        * Método de consulta del atributo studies.
+        * 
+        * @return Devuelve los estudios del curso
+        */
+        function getStudies() {
+            return $this->studies;
+        }
+
+        /*
         * Método de consulta del atributo header_image.
         * 
-        * @return Devuelve la imagen de cabecera del módulo
+        * @return Devuelve la imagen de cabecera del curso
         */
         function getHeaderImage() {
             return $this->header_image;
@@ -62,7 +74,7 @@
         /*
         * Método de consulta del atributo preview.
         * 
-        * @return Devuelve el contenido de previsualización del módulo
+        * @return Devuelve el contenido de previsualización del curso
         */
         function getPreview() {
             return $this->preview;
@@ -71,7 +83,7 @@
         /*
         * Método de consulta del atributo content.
         * 
-        * @return Devuelve el contenido del módulo
+        * @return Devuelve el contenido del curso
         */
         function getContent() {
             return $this->content;
