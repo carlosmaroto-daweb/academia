c4a0e64d9b96fef31baa170385721090d2037b93
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-12 15:11:48 +0200

    v1.0

--- model/moduleManagement.php
+++ model/moduleManagement.php
@@ -2,9 +2,11 @@
     // Incluimos el archivo db.php para instanciar la clase como objeto,
     // esta clase va a gestionar la comunicación con la base de datos.
     require_once 'model/db.php';
+
     // Incluimos el archivo module.php para instanciar la clase como objeto,
     // esta clase define los campos que van a tener los módulos.
     require_once 'model/module.php';
+    
     // Incluimos el archivo user.php para instanciar la clase como objeto,
     // esta clase define los campos que van a tener los usuarios.
     require_once 'model/user.php';
