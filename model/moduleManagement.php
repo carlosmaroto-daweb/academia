c33237f91bd0149c8795dbcec723d7f4aef00542
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-12 20:14:08 +0200

    v1.0

--- model/moduleManagement.php
+++ model/moduleManagement.php
@@ -7,12 +7,31 @@
     // esta clase define los campos que van a tener los módulos.
     require_once 'model/module.php';
 
+    /* 
+    * Esta clase hace de intermediaria entre el controlador courseController
+    * con funcionalidades de gestión de módulos, y modelos como el de la base 
+    * de datos db y el modelo que define los campos de los módulos module.
+    * De forma que termina de ejecutar las funcionalidades implementadas en el
+    * controlador courseController como crear, duplicar, editar y eliminar
+    * módulos, haciendo comprobaciones con la lista de módulos actuales de la
+    * base de datos y posteriormente llamar a los métodos de la base de datos 
+    * para terminar el proceso y devolver el resultado.
+    * 
+    * @author: Carlos Maroto
+    * @version: v1.0.0 Carlos Maroto
+    */
     class ModuleManagement {
         
         // Atributos
         private $db;
         private $modules;
 
+        /*
+        * Creamos una instancia de Db al inicio para poder utilizarla más 
+        * adelante, a su vez el constructor de esta clase crea una conexión
+        * con la base de datos. Además actualiza la lista de módulos, por
+        * lo que siempre estamos trabajando con los nuevos datos.
+        */
         function __construct() {
             $this->db = new Db();
             $this->updateModules();
@@ -108,6 +127,12 @@
             return $result;
         }
 
+        /*
+         * Método que devuelve el módulo dado por su id.
+         * 
+         * @param  String id del módulo a consultar.
+         * @return Devuelve el módulo dado su id o null si no se encuentra.
+        */
         function getModuleById($id) {
             $result = null;
             for ($i=0; $i<count($this->modules) && !$result; $i++) {
@@ -118,6 +143,12 @@
             return $result;
         }
 
+        /*
+         * Método que devuelve el módulo dado por su nombre.
+         * 
+         * @param  String nombre del módulo a consultar.
+         * @return Devuelve el módulo dado su nombre o null si no se encuentra.
+        */
         private function getModuleByName($name) {
             $result = null;
             for ($i=0; $i<count($this->modules) && !$result; $i++) {
@@ -128,10 +159,20 @@
             return $result;
         }
 
+        /*
+        * Método de consulta que devuelve el array privado modules que contiene
+        * la lista de todos los módulos actuales de la base de datos.
+        * 
+        * @return Lista de los módulos de la base de datos.
+        */
         function getModules() {
             return $this->modules;
         }
 
+        /*
+         * Método que guarda en el array modules los datos de los módulos
+         * registados en la base de datos.
+        */
         private function updateModules() {
             $this->modules = [];
             $query = $this->db->getModules();
