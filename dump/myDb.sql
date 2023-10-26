8feca435cb81f8bfc0391920d4380a36f238f9a9
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-26 17:23:31 +0200

    v1.0

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -4,7 +4,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255),
-  `password` VARCHAR(255),
+  `password` CHAR(60),
   `name` VARCHAR(255),
   `last_name` VARCHAR(255),
   `phone_number` VARCHAR(255),
@@ -16,15 +16,15 @@ CREATE TABLE `user` (
 CREATE TABLE `lesson` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255),
-  `files` LONGBLOB,
+  `files` VARCHAR(255),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `module` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255),
-  `header_image` LONGBLOB,
-  `preview` LONGBLOB,
-  `content` LONGBLOB,
+  `header_image` LONGTEXT,
+  `preview` LONGTEXT,
+  `content` LONGTEXT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
