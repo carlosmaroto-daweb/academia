792da6ff8758adf234c8c62112b62f4a86d7f34b
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-27 17:59:14 +0200

    v1.0

--- www/controller/courseController.php
+++ www/controller/courseController.php
@@ -126,6 +126,29 @@
               if (isset($_POST['title-'.$i]) && !empty($_POST['title-'.$i])) {
                 $title = $_POST['title-'.$i];
                 if (isset($_FILES[$title]) && !empty($_FILES[$title])) {
+                  
+                  $file = $_FILES[$title]["name"]; //Nombre de nuestro archivo
+                  $url_temp = $_FILES[$title]["tmp_name"]; //Ruta temporal a donde se carga el archivo 
+
+                  //dirname(__FILE__) nos otorga la ruta absoluta hasta el archivo en ejecución
+                  $url_insert = "view/assets/academia/files"; //Carpeta donde subiremos nuestros archivos
+
+                  //Ruta donde se guardara el archivo, usamos str_replace para reemplazar los "\" por "/"
+                  $url_target = str_replace('\\', '/', $url_insert) . '/' . $file;
+
+                  //Si la carpeta no existe, la creamos
+                  if (!file_exists($url_insert)) {
+                    mkdir($url_insert, 0777, true);
+                  };
+
+                  //movemos el archivo de la carpeta temporal a la carpeta objetivo y verificamos si fue exitoso
+                  if (move_uploaded_file($url_temp, $url_target)) {
+                    echo "El archivo " . htmlspecialchars(basename($file)) . " ha sido cargado con éxito.";
+                  } else {
+                    echo "Ha habido un error al cargar tu archivo.";
+                  }
+
+
                   // Guardar el archivo $_FILES[$title] y obtener el path del archivo
                   $path = '';
                   $result += $title . ';;' . $path . ';;';
