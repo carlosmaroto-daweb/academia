0a089becac550d9a4551b919128d15a2886cd225
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-10 16:30:39 +0100

    v1.0

--- www/config/config.php
+++ www/config/config.php
@@ -1,18 +1,18 @@
 <?php
-    // Constantes de la base de datos
-    define("DB_HOST", "db");
-    define("DB", "academia");
-    define("DB_USER", "u-Area14");
-    define("DB_PASS", "u-Area14");
+	// Constantes de la base de datos
+	define("DB_HOST", "db");
+	define("DB", "academia");
+	define("DB_USER", "u-Area14");
+	define("DB_PASS", "u-Area14");
 
-    // Constantes de la ruta por defecto
-    define("DEFAULT_CONTROLLER", "userController");
-    define("DEFAULT_ACTION", "index");
+	// Constantes de la ruta por defecto
+	define("DEFAULT_CONTROLLER", "userController");
+	define("DEFAULT_ACTION", "index");
 
-    // Constantes del encriptamiento
-    define("PASSWORD_HASH", PASSWORD_DEFAULT);
-    define("PASSWORD_COST", 14);
+	// Constantes del encriptamiento
+	define("PASSWORD_HASH", PASSWORD_DEFAULT);
+	define("PASSWORD_COST", 14);
 
-    // Constantes de la ruta de los archivos video y pdf
-    define("DEFAULT_UPDATE", "view/assets/academia/files");
+	// Constantes de la ruta de los archivos video y pdf
+	define("DEFAULT_UPDATE", "view/assets/academia/files");
 ?>
