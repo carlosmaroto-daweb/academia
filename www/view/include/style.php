0a59efcc6c5c46c97dae8f0d552c39a2d4cb479a
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-13 11:36:11 +0100

    v1.0

--- www/view/include/style.php
+++ www/view/include/style.php
@@ -4,12 +4,12 @@
 <!-- CSS -->
 <link rel="stylesheet" href="view/assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="view/assets/css/style.css">
-<link rel="stylesheet" href="view/assets/css/style-responsive.css">
+<!--<link rel="stylesheet" href="view/assets/css/style-responsive.css">-->
 <link rel="stylesheet" href="view/assets/css/vertical-rhythm.min.css">
 <link rel="stylesheet" href="view/assets/css/magnific-popup.css">
 <link rel="stylesheet" href="view/assets/css/owl.carousel.css">
 <link rel="stylesheet" href="view/assets/css/animate.min.css">
-<link rel="stylesheet" href="view/assets/css/splitting.css">
+<!--<link rel="stylesheet" href="view/assets/css/splitting.css">-->
 
 <link rel="stylesheet" href="view/assets/academia/summernote/summernote.min.css">
 <link rel="stylesheet" href="view/assets/academia/css/style.css">
