f16c3cd6940ffbc9582fad2a93a9b0b8a1c0d369
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-15 12:02:58 +0100

    v1.0

--- www/view/editSubject.php
+++ www/view/editSubject.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>Academia Cartabón | Edición módulo</title>
+        <title>Academia Cartabón | Edición asignatura</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         
@@ -32,18 +32,18 @@
                         
                         <div class="col-md-8">
                             <div class="wow fadeInUpShort" data-wow-delay=".1s">
-                                <h1 class="hs-line-7 mb-20 mb-xs-10">Edición de módulo</h1>
+                                <h1 class="hs-line-7 mb-20 mb-xs-10">Edición de asignatura</h1>
                             </div>
                             <div class="wow fadeInUpShort" data-wow-delay=".2s">
                                 <p class="hs-line-6 opacity-075 mb-20 mb-xs-0">
-                                    El contenido editado se mostrará en el módulo específico
+                                    El contenido editado se mostrará en la asignatura específica
                                 </p>
                             </div>
                         </div>
                         
                         <div class="col-md-4 mt-30 wow fadeInUpShort" data-wow-delay=".1s">
                             <div class="mod-breadcrumbs text-end">
-                                <a href="index.php">Inicio</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<a href="index.php?controller=courseController&action=secretary">Secretaría</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<span>Edición módulo</span>
+                                <a href="index.php">Inicio</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<a href="index.php?controller=courseController&action=secretary">Secretaría</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<span>Edición asignatura</span>
                             </div>                                
                         </div>
                         
@@ -54,43 +54,43 @@
             <!-- End Home Section -->
         
             <?php
-                $module  = $dataToView["module"];
+                $subject  = $dataToView["subject"];
                 $id           = "";
                 $name         = "";
                 $header_image = "";
                 $preview      = "";
                 $content      = "";
-                $assigned_lessons = [];
-                if ($module) {
-                    $id            = $module->getId();
-                    $name          = $module->getName();
-                    $header_image  = $module->getHeaderImage();
-                    $preview       = $module->getPreview();
-                    $content       = $module->getContent();
-                    $module_lesson = $dataToView["module_lesson"];
-                    for ($i=0; $i<count($module_lesson); $i++) { 
-                        if ($module_lesson[$i][0]->getId() == $id) {
-                            array_push($assigned_lessons, $module_lesson[$i][1]->getId());
+                $assigned_modules = [];
+                if ($subject) {
+                    $id            = $subject->getId();
+                    $name          = $subject->getName();
+                    $header_image  = $subject->getHeaderImage();
+                    $preview       = $subject->getPreview();
+                    $content       = $subject->getContent();
+                    $subject_module = $dataToView["subject_module"];
+                    for ($i=0; $i<count($subject_module); $i++) { 
+                        if ($subject_module[$i][0]->getId() == $id) {
+                            array_push($assigned_modules, $subject_module[$i][1]->getId());
                         }
                     }
                 }
-                $lessons = $dataToView["lessons"];
-                $options_lessons = "<option>No hay lecciones.</option>";
-                if ($lessons) {
-                    $options_lessons = "";
-                    foreach ($lessons as $lesson):
-                        $options_lessons .= '<option value="' . $lesson->getId() . '">(' . $lesson->getId() . ') ' . $lesson->getName() . '</option>';
+                $modules = $dataToView["modules"];
+                $options_modules = "<option>No hay módulos.</option>";
+                if ($modules) {
+                    $options_modules = "";
+                    foreach ($modules as $module):
+                        $options_modules .= '<option value="' . $module->getId() . '">(' . $module->getId() . ') ' . $module->getName() . '</option>';
                     endforeach;
                 }
                 echo "<section class='page-section'>";
                     echo "<div class='container'>";
-                        echo "<form id='module-edit-form' class='form'>";
-                            echo "<input id='module-edit-id' type='hidden' name='id' value='{$id}'>";
+                        echo "<form id='subject-edit-form' class='form'>";
+                            echo "<input id='subject-edit-id' type='hidden' name='id' value='{$id}'>";
                             echo "<div id='preview_image'></div>";
                             echo "<div id='content_image'></div>";
                             echo "<div class='form-group'>";
-                                echo "<label for='module-edit-name'>Nombre <span class='required-field-color'>(*)</span></label>";
-                                echo "<input id='module-edit-name' type='text' name='name' value='{$name}' class='input-md round form-control'>";
+                                echo "<label for='subject-edit-name'>Nombre <span class='required-field-color'>(*)</span></label>";
+                                echo "<input id='subject-edit-name' type='text' name='name' value='{$name}' class='input-md round form-control'>";
                             echo "</div>";
                             echo "<div class='form-group'>";
                                 echo "<label for='header_image'>Foto de cabecera <span class='required-field-color'>(*)</span></label>";
@@ -106,34 +106,34 @@
                                 echo "<div id='content'>{$content}</div>";
                             echo "</div>";
                             echo "<div class='form-group'>";
-                                echo "<label>Lecciones asignadas</label>";
-                                echo "<div id='container-assigned_lessons'>";
-                                    if ($assigned_lessons) {
-                                        foreach ($assigned_lessons as $assigned_lesson):
-                                            $options_lessons_selected = "";
-                                            foreach ($lessons as $lesson):
+                                echo "<label>Módulos asignados</label>";
+                                echo "<div id='container-assigned_modules'>";
+                                    if ($assigned_modules) {
+                                        foreach ($assigned_modules as $assigned_module):
+                                            $options_modules_selected = "";
+                                            foreach ($modules as $module):
                                                 $stringSelected = '';
-                                                if ($lesson->getId() == $assigned_lesson) {
+                                                if ($module->getId() == $assigned_module) {
                                                     $stringSelected = ' selected ';
                                                 }
-                                                $options_lessons_selected .= '<option value="' . $lesson->getId() . '"' . $stringSelected . '>(' . $lesson->getId() . ') ' . $lesson->getName() . '</option>';
+                                                $options_modules_selected .= '<option value="' . $module->getId() . '"' . $stringSelected . '>(' . $module->getId() . ') ' . $module->getName() . '</option>';
                                             endforeach;
-                                            echo "<div class='row-assigned_lessons'>";
-                                                echo "<div class='grid-row-assigned_lessons btn btn-mod btn-circle'><i class='fa fa-grip-vertical'></i></div>";
-                                                echo "<select class='assigned_lessons input-md round form-control'>{$options_lessons_selected}</select>";
-                                                echo "<div class='delete-row-assigned_lessons btn btn-mod btn-circle button-cancel'><i class='fa fa-times'></i></div>";
+                                            echo "<div class='row-assigned_modules'>";
+                                                echo "<div class='grid-row-assigned_modules btn btn-mod btn-circle'><i class='fa fa-grip-vertical'></i></div>";
+                                                echo "<select class='assigned_modules input-md round form-control'>{$options_modules_selected}</select>";
+                                                echo "<div class='delete-row-assigned_modules btn btn-mod btn-circle button-cancel'><i class='fa fa-times'></i></div>";
                                             echo "</div>";
                                         endforeach;
                                     }
                                 echo "</div>";
-                                echo "<a id='add-row-assigned_lessons' class='btn btn-mod btn-circle button-success' data-options_lessons='{$options_lessons}'><i class='fa fa-plus'></i></a>";
+                                echo "<a id='add-row-assigned_modules' class='btn btn-mod btn-circle button-success' data-options_modules='{$options_modules}'><i class='fa fa-plus'></i></a>";
                             echo "</div>";
                         echo "</form>";
                         echo "<div class='container mt-50'>";
                             echo "<div class='row'>";
                                 echo "<div class='col-md-12 text-center'>";
                                     echo "<a href='index.php?controller=courseController&action=secretary' class='btn btn-mod btn-circle btn-large button-cancel'>Cancelar</a>&nbsp;";
-                                    echo "<a id='edit-module' class='btn btn-mod btn-circle btn-large button-success'>Guardar</a>";
+                                    echo "<a id='edit-subject' class='btn btn-mod btn-circle btn-large button-success'>Guardar</a>";
                                 echo "</div>";
                             echo "</div>";
                         echo "</div>";
