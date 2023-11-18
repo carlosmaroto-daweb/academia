42b6a95c71e3ce0f168b33e0de80f701c598ca4e
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-18 18:47:58 +0100

    v1.0

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -63,3 +63,8 @@ CREATE TABLE `subject_module` (
   PRIMARY KEY (`id_subject`, `id_module`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `course_user` (
+  `id_course` INT NOT NULL,
+  `id_user` INT NOT NULL,
+  PRIMARY KEY (`id_course`, `id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
