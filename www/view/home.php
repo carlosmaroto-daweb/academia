5782907592a39fbf395003ecdf907bebe9226c5c
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-23 17:57:48 +0100

    v1.0

--- www/view/home.php
+++ www/view/home.php
@@ -97,13 +97,13 @@
                                                 }
                                             }
                                 ?>
-                                            <!-- Post -->
+                                            <!-- Course -->
                                             <div class="blog-item">
                                                 
-                                                <!-- Post Title -->
+                                                <!-- Course Title -->
                                                 <h2 class="blog-item-title"><a href="blog-single-sidebar-right.html"><?php echo $course->getName();?></a></h2>
                                                 
-                                                <!-- Author, Categories, Comments -->
+                                                <!-- Tags and Info -->
                                                 <div class="blog-item-data">
                                                     <a href=""><i class="fa fa-tags"></i> <?php echo $course->getStudies();?></a>
                                                     <span class="separator">&nbsp;</span>
